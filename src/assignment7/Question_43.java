0162ae2630447b2cb63319cbae648a95b161f81a
SAILEISH <saileshtimilsena.01@gmail.com>
2021-03-29 20:34:02 -0500

    Updated assignment

--- src/assignment7/Question_43.java
+++ src/assignment7/Question_43.java
@@ -2,4 +2,20 @@ package assignment7;
 
 public class Question_43 {
 
+    public static void main(String[] args)
+    {
+        try {
+            int number[] = new int[10];
+            number[10] = 50 / 0;
+        }
+        catch (ArithmeticException e) {
+            System.out.println(
+                    "Zero cannot divide any number");
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(
+                    "Index out of size of the array");
+        }
+    }
+
 }
